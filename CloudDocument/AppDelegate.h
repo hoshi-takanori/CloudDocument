a4a9b00f131a3b092482926c900b94c7964f20c8
Hoshi Takanori <hoshi@hoshi-air13.local>
2012-01-28 01:15:09 +0900

    Initial Commit

---
+++ CloudDocument/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CloudDocument
+//
+//  Created by Hoshi Takanori on 12/01/28.
+//  Copyright (c) 2012年 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CloudDocument/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CloudDocument
+//
+//  Created by Hoshi Takanori on 12/01/28.
+//  Copyright (c) 2012年 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
